f07f1b1a0831dd88463e63e4160d92af0930f325
Tony <120454698+Devshauntest@users.noreply.github.com>
2023-03-17 21:29:10 +0000

    Add files via upload

---
+++ Basic_JavaScript_Projects/Project1_Expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("I am putting to test my JS skill"); // window.alert() method
+document.write("I am loving my javascript journey") //document.write() method
+
+let sent1 = "This is the beginning of the string "; // this is the first variable
+let sent2 = "and this is the end of the string" // this is the second variable  
+document.write(sent1 + sent2) // concatenation
+
+document.write(7 * 5) //expression
+
